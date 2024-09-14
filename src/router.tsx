985993c686ac2cce0da7e8bfeda329bdb3f3b483
misterdan100 <danielmca60@gmail.com>
2024-09-14 05:44:20 -0500

    core: use lazy and suspense from react to improve performance

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,27 @@
+import { lazy, Suspense } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import IndexPage from './views/IndexPage'
-import FavoritesPage from './views/FavoritesPage'
 import Layout from './layouts/Layout'
+
+// import IndexPage from './views/IndexPage'
+// import FavoritesPage from './views/FavoritesPage'
+const FavoritesPage = lazy(() => import('./views/FavoritesPage')) // improve performance
+const IndexPage = lazy(() => import('./views/IndexPage')) // improve performance
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
         <Routes>
             <Route element={<Layout />}>
-                <Route path='/' element={<IndexPage />} index/>
-                <Route path='/favorites' element={<FavoritesPage />}/>
+                <Route path='/' element={
+                  <Suspense fallback='Loading...'>
+                    <IndexPage />
+                  </Suspense>
+                } index/>
+                <Route path='/favorites' element={
+                  <Suspense fallback='Loading...'>
+                      <FavoritesPage />
+                  </Suspense>
+                }/>
             </Route>
         </Routes>
     </BrowserRouter>
