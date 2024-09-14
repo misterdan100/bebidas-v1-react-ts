d94ddb99e2cb57e14464361a8b6533eda6f44c3e
misterdan100 <danielmca60@gmail.com>
2024-09-14 04:24:05 -0500

    feat: create favorite page and show favorite drinks

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,9 +1,22 @@
-import Header from "../components/Header";
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
 
     export default function FavoritesPage() {
+      const favorites = useAppStore(state => state.favorites)
+
+
   return (
     <>
+      <h1 className="text-4xl font-bold">Favorites</h1>
 
+      <div className="grid grid-cols-2 gap-5 my-5 md:grid-cols-4 2xl:grid-cols-5">
+        {favorites.map( recipe => (
+          <DrinkCard 
+            key={recipe.idDrink}
+            drink={recipe}
+          />
+        ))}
+      </div>
     </>
   )
 }
